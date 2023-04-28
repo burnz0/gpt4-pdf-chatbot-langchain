03a2ae5f6d599ddcc0b02c56b26f19f691431996
Alexander Linke <burnz0@proton.me>
2023-04-28 09:41:42 +0200

    init changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,8 @@ export default function Home() {
   }>({
     messages: [
       {
-        message: 'Hi, what would you like to learn about this legal case?',
+        // print message with the source document name
+        message: 'Hallo, ich bin der Chatbot LangChainAI. Wie kann ich dir helfen?',
         type: 'apiMessage',
       },
     ],
@@ -48,7 +49,7 @@ export default function Home() {
     setError(null);
 
     if (!query) {
-      alert('Please input a question');
+      alert('Bitte eine Frage eingeben.');
       return;
     }
 
@@ -125,7 +126,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat With Your Legal Docs
+            Chatbot LangChainAI / GPT-3.5-turbo
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -188,14 +189,14 @@ export default function Home() {
                               <div key={`messageSourceDocs-${index}`}>
                                 <AccordionItem value={`item-${index}`}>
                                   <AccordionTrigger>
-                                    <h3>Source {index + 1}</h3>
+                                    <h3>Quelle {index + 1}</h3>
                                   </AccordionTrigger>
                                   <AccordionContent>
                                     <ReactMarkdown linkTarget="_blank">
                                       {doc.pageContent}
                                     </ReactMarkdown>
                                     <p className="mt-2">
-                                      <b>Source:</b> {doc.metadata.source}
+                                      <b>Quelle:</b> {doc.metadata.source}
                                     </p>
                                   </AccordionContent>
                                 </AccordionItem>
@@ -223,8 +224,8 @@ export default function Home() {
                     name="userInput"
                     placeholder={
                       loading
-                        ? 'Waiting for response...'
-                        : 'What is this legal case about?'
+                        ? 'Information wird gesucht...'
+                        : 'Welche Information möchtest du erhalten?'
                     }
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
@@ -261,8 +262,8 @@ export default function Home() {
           </main>
         </div>
         <footer className="m-auto p-4">
-          <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
+          <a href="https://github.com/mayooear/gpt4-pdf-chatbot-langchain">
+            Powered by LangChainAI. Built on Demo of gpt4-pdf-chatbot-langchain by Mayo (mayooear).
           </a>
         </footer>
       </Layout>
